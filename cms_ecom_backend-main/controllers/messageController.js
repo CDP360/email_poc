e3a891b3e84a8e59ed66c554840afbd94237aa20
vimal_ROG <vimal@cdp360.com>
2022-11-03 14:03:54 +0530

    wish api get data

--- cms_ecom_backend-main/controllers/messageController.js
+++ cms_ecom_backend-main/controllers/messageController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const ChatRoom = require("../models/ChatRoom");
 const User = require("../models/User");
+const Product = require("../models/Product");
 const messageService = require("../services/messageService");
 // mail client
 let nodemailer = require("nodemailer");
@@ -75,6 +76,27 @@ router.post("/emailsent", async (req, res) => {
   });
 
   //   res.status(200).json({ sender: "mail sent" });
+});
+
+router.get("/cartlist", async (req, res) => {
+  // const { chatId, message } = req.body;
+  // console.log(req.body.user_id);
+
+  let userFound = await User.findOne({ _id: req.body.user_id });
+
+  var wish = JSON.stringify(userFound);
+  var getID = JSON.parse(wish)["wishedProducts"];
+  console.log("hi", getID);
+  var arrayWish = [];
+  for (var key in getID) {
+    console.log("key", getID[key]);
+    let itemFound = await Product.findById(getID[key]);
+    console.log(itemFound);
+    // let wish1 = JSON.parse(itemFound)
+    // let getID1 = JSON.stringify(wish1)
+    arrayWish.push(itemFound);
+  }
+  res.status(200).json(arrayWish);
 });
 
 module.exports = router;
